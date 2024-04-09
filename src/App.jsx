b4e57a62c9264b63d25a342878fa520bd5721a02
Ómar Saldarriaga <saldarriagaomar@gmail.com>
2024-04-09 10:28:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='headName'>
+        <h1>PROCONDET SAS</h1>
+      </div>
+      <div>
+        <div className='navMen'>
+          <button>Inicio</button>
+          <button>Servicios</button>
+          <button>Productos</button>
+          <button>Contacto</button>
+        </div>
+      </div>
+      <div className='body'>
+        <div className='whatProcondet'>
+          <h1>Somos Procondet SAS</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias temporibus enim voluptates recusandae deserunt nostrum accusantium eaque nemo numquam maiores laborum, provident, natus nam esse, doloremque aliquid est in aspernatur?</p>
+        </div>
+        <div className='whyProcondet'>
+          <h1>¿Por qué elegir Procondet SAS?</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste eius, molestiae nulla excepturi, accusamus ipsa aspernatur nihil dignissimos veritatis consequatur cumque a odio necessitatibus hic voluptatibus deleniti asperiores optio consequuntur?</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
